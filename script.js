e8f6a196ddaa1e30b1a4153eadc6d0b76414af4d
lazaroPedro <69309196+lazaroPedro@users.noreply.github.com>
2022-09-11 21:38:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+/*console.log('Hello!');
+
+const activeClass = 'active';
+const list = document.querySelectorAll('li.navItem');
+
+addEventListener(click, document.i)*
+
+
+
+----------
+
+
+const list = document.querySelectorAll('.navItem')
+function activeLink(){
+  list.forEach((item) => 
+  item.classList.remove('active'))
+  this.classList.add('active')
+}
+list.forEach((item) =>
+item.addEventListener('mousemove', activeLink))
+
+
+------------
+
+
+
+
+/*
+
+$("#homeLink").click(function() {
+  $('html, body').animate({
+      scrollTop:        $("#home").offset().top-66
+  }, 1000);
+return false;
+});
+
+$("#aboutLink").click(function() {
+  $('html, body') .animate({
+      scrollTop:        $("#about-us").offset().top-112
+  }, 1000);
+return false;
+});*/
